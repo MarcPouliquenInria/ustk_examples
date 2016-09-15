fb6df58c688bd99ba1b690f546b6e1b4349f3553
marc pouliquen <mpouliqu@ustk.irisa.fr>
2016-09-15 17:23:58 +0200

    changing test for usPrescan3D

--- usImageTests/usPrescan3D.cpp
+++ usImageTests/usPrescan3D.cpp
@@ -4,27 +4,37 @@ int main(int argc, const char** argv) {
   //prescan image of uchar type
   std::cout << "Testing usImagePreScan3D<unsigned char> : " << std::endl;
   usImage3D<unsigned char> imgUChar;
-  usImagePreScan3D<unsigned char> imagePreScan3DUChar(imgUChar, usImageSettings3D());
+  usImageSettings3D settingsUChar;
+  usImagePreScan3D<unsigned char> imagePreScan3DUChar(imgUChar, settingsUChar);
+  //print default values
+  std::cout << "probe default info : " << std::endl;
+  imagePreScan3DUChar.printProbeSettings();
   imagePreScan3DUChar.setProbeRadius(40.10f);
   imagePreScan3DUChar.setBSampleFreq(10.5f);
   imagePreScan3DUChar.setFrameAngle(15.3f);
   imagePreScan3DUChar.setLineAngle(55.84f);
   imagePreScan3DUChar.setMotorRadius(5.54f);
   imagePreScan3DUChar.setProbeElementPitch(0.05f);
-  std::cout << "probe info : " << std::endl;
+  //print updated values
+  std::cout << "probe new info : " << std::endl;
   imagePreScan3DUChar.printProbeSettings();
 
   //prescan image of double type
   std::cout << "Testing usImagePreScan3D<double> : " << std::endl;
   const usImage3D<double> img;
-  usImagePreScan3D<double> imagePreScan3DDouble(img, usImageSettings3D());
+  usImageSettings3D settingsDouble;
+  usImagePreScan3D<double> imagePreScan3DDouble(img, settingsDouble);
+  //print default values
+  std::cout << "probe info : " << std::endl;
+  imagePreScan3DDouble.printProbeSettings();
   imagePreScan3DDouble.setProbeRadius(40.10f);
   imagePreScan3DDouble.setBSampleFreq(10.5f);
   imagePreScan3DDouble.setFrameAngle(15.3f);
   imagePreScan3DDouble.setLineAngle(55.84f);
   imagePreScan3DDouble.setMotorRadius(5.54f);
   imagePreScan3DDouble.setProbeElementPitch(0.05f);
-  std::cout << "probe info : " << std::endl;
+  //print updated values
+  std::cout << "probe new info : " << std::endl;
   imagePreScan3DDouble.printProbeSettings();
 
   exit(EXIT_SUCCESS);
