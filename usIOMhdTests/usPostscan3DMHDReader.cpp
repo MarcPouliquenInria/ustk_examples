8d36b7290d1b32ec678bce6a525b7f380fe2d83e
marc pouliquen <marc.pouliquen@inria.fr>
2016-09-23 16:48:01 +0200

    reading mhd files

--- usIOMhdTests/usPostscan3DMHDReader.cpp
+++ usIOMhdTests/usPostscan3DMHDReader.cpp
@@ -4,6 +4,6 @@
 int main(int argc, const char** argv) {
   std::cout << "test 1" << std::endl;
   usImageIo img;
-  img.readPostScan3D(std::string(argv[2])); //C:\Users\mpouliqu\Documents\DataUS\postscan\3D\postscan3d.mhd
+  img.readPostScan3D(std::string("/home/mpouliqu/Documents/usData/postscan/3D/postscan3d.mhd")); //C:\Users\mpouliqu\Documents\DataUS\postscan\3D\postscan3d.mhd
   exit(EXIT_SUCCESS);
 }
