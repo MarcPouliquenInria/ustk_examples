b54c2bbecbba8520c2c737e614a9cd2770314408
marc pouliquen <marc.pouliquen@inria.fr>
2016-09-29 18:12:02 +0200

    adding read/write for mhd and raw files

--- usIOMhdTests/usPostscan3DMHDReader.cpp
+++ usIOMhdTests/usPostscan3DMHDReader.cpp
@@ -4,8 +4,21 @@
 int main(int argc, const char** argv) {
   usImageIo img;
   usImagePostScan3D<unsigned char> postScanImage3D;
-  img.read(postScanImage3D,std::string("/home/mpouliqu/Documents/usData/ustk-tests-dataset/postscan3d.mhd"));
+  usImageIo::read(postScanImage3D,std::string("postscan3d.mhd"));
 
-  std::cout << "scanline pitch: " << postScanImage3D.getScanLinePitch() << std::endl;
+
+
+    std::cout << "dim: " << postScanImage3D.getDimX() << ", " << postScanImage3D.getDimY() << ", " << postScanImage3D.getDimZ() << std::endl;
+    std::cout << "elementSpacing: " << postScanImage3D.getElementSpacingX() << ", " << postScanImage3D.getElementSpacingY() << ", " << postScanImage3D.getElementSpacingZ() << std::endl;
+
+    std::cout << "imageConvex: " << postScanImage3D.isImageConvex() << std::endl;
+    std::cout << "motorConvex: " << postScanImage3D.isMotorConvex() << std::endl;
+    std::cout << "probeRadius: " << postScanImage3D.getProbeRadius() << std::endl;
+    std::cout << "scanLinePitch: " << postScanImage3D.getScanLinePitch() << std::endl;
+    std::cout << "motorradius: " << postScanImage3D.getMotorRadius() << std::endl;
+    std::cout << "framePitch: " << postScanImage3D.getFramePitch() << std::endl;
+
+
+    usImageIo::write(postScanImage3D,"test");
   exit(EXIT_SUCCESS);
 }
